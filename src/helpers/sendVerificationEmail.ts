9c9e21d31d1fba4615c3ce8b3ad5a25b28467f18
stefanbinoj <stefan.binoj.007@gmail.com>
2024-11-17 17:41:04 +0530

    type safe api res

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,23 @@
 import {resend} from "@/lib/resend"
-import VerificationEmail from "../../emails/verificationEmail"
+import VerificationEmail from "../../emails/verificationEmail";
+import { ApiResponse } from "@/types/apiResponses";
+
+export async function sendVerificationEmail(
+    username : string,
+    verificationCode : string,
+    email : string
+):Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Mystery Message | Verification Code',
+            react: VerificationEmail({username , otp:verificationCode}),
+          });
+
+        return {message:"Email send succesfully", success:true}
+    } catch (error) {
+        console.log("Eror sending verification email  : "+error)
+        return {message:"Error Failed to send email " +error , success:false}
+    }
+}
---
+++ src/types/apiResponses.ts
@@ -0,0 +1,9 @@
+import { Message } from "@/models/User.model";
+
+export interface ApiResponse{
+    success:boolean;
+    message:string;
+    isAcceptingMessage?:boolean;
+    messages?:Array<Message>;
+
+}
