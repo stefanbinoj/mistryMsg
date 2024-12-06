fe7a8a8281cf930cb76b16521533cb382063dec0
stefanbinoj <stefan.binoj.007@gmail.com>
2024-12-06 17:56:33 +0530

    nav-fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,23 +41,19 @@ const Navbar = () => {
     const user : User = session?.user as User
     console.log("hamara user in nav is : ",user)
   return (
-    <nav className={`p-6 py-4 transparent shadow-lg mx-2 sticky top-0 backdrop-blur-sm ${isDarkMode ? 'bg-black text-white' : 'bg-white text-black'} opacity-100`}>
-        <div className='container mx-auto flex flex-col 
-        md:flex-row justify-between items-center '>
-            <a className='font-bold ml-4' href='/'>Mystery Message</a>
-            {   session ? 
-                <>
-
-                <a  href='/dashboard' className='font-medium'>{window.location.pathname=='/' ? "Dashboard" : "Home"}</a>
-                <Button onClick={()=>signOut()}>Logout</Button> 
-                </>  : <><Link href="/sign-in">
-                <Button className='bg-slate-200	' >Login</Button>
-                </Link>
-                </>
-            }
-            <div className="flex items-center space-x-2">
-              <Switch   onCheckedChange={toggleTheme}  checked={isDarkMode} id="airplane-mode" />
-              <Label htmlFor="airplane-mode">Dark Mode</Label>
+    <nav className={`pl-5 pr-0 py-5 mx-2 z-50 shadow-lg sticky top-0 backdrop-blur-sm border-b  ${isDarkMode ? 'bg-black/30 text-white border-gray-800' : 'bg-white text-black border-gray-300'}`}>
+        <div className='container w-full 
+        md:flex flex-row gap-4 items-center ml-5 w-screen justify-between '>
+            <a className={`font-bold text-xl ml-4 ${!isDarkMode?'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text':'bg-gradient-to-r from-orange-400 to-red-400 text-transparent bg-clip-text'} hover:scale-125 transition-all duration-300 ease-in-out`} href='/'>Mystery Message</a>
+            <div className=' hidden md:flex md:flex-row md:block w-3/5 gap-4 ml-auto mr-0 justify-end'>
+              
+              <a className=' cursor-pointer hover:scale-125 transition-all hover:font-bold duration-300 ease-in-out' href='/dashboard'>Dashboard</a>
+              {session? <a className=' cursor-pointer  hover:scale-125 transition-all hover:font-bold duration-300 ease-in-out ' onClick={()=>signOut()}>Logout</a> 
+                 : <a className=' cursor-pointer  hover:scale-125 transition-all hover:font-bold duration-300 ease-in-out ' href="/sign-in"> Loign
+                </a>}
+            </div>
+            <div className="flex items-center space-x-2 ml-auto item-end cursor-pointer">
+              <Switch className='hover:scale-125 transition-all duration-300 ease-in-out'   onCheckedChange={toggleTheme}  checked={isDarkMode} id="airplane-mode" />
             </div>
         </div>
     </nav>
