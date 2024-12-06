7440c7ae8d3827a2bbf549987962d4c710e8d658
stefanbinoj <stefan.binoj.007@gmail.com>
2024-12-06 12:52:37 +0530

    send message fix

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,16 +1,21 @@
 "use client";
+import { useToast } from "@/hooks/use-toast";
+import { ApiResponse } from "@/types/apiResponses";
+import axios, { AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
-import { useForm , SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type FormData = {
   textArea: string;
 };
 
 const page = () => {
-  const [usernmae, setusername] = useState<string>("");
+  const {toast} = useToast();
+  const [username, setUsername] = useState<string>("");
+
   useEffect(() => {
     const pathname = window.location.pathname.split("/");
-    setusername(pathname[2]);
+    setUsername(pathname[2]);
   }, []);
 
   const {
@@ -19,30 +24,74 @@ const page = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
-  const onSubmit = (data :FormData) => console.log(data);
 
+  const onSubmit: SubmitHandler<FormData> = async(data : FormData) => {
+    setValue("textArea", ""); 
+      try {
+          const result = await axios.post<ApiResponse>(`/api/send-message`,{
+              username:username,
+              content : data.textArea
+
+          })
+          if(result.status===400 ){
+            toast({
+              title:"Oopss",
+              description:"user not receiving Messages now",
+              variant:"destructive"
+          })
+          }
+          else if(result.status===404){
+            toast({
+              title:"success",
+              description:result.data.message,
+              variant:"destructive"
+          })
+          }
+          else{
+            toast({
+                title:"success",
+                description:result.data.message
+            })
+          }
+
+      } catch (error) {
+          console.log("error in verifying of user " , error)
+          const axiosError = error as AxiosError<ApiResponse>;
+          let errorMessage = axiosError.response?.data.message;
+          toast({
+              title:"Error",
+              description : errorMessage,
+              variant: "destructive"
+          })
+      }
+  
+  };
 
   return (
     <>
-      <div className="md:py-10 md:px-20 flex flex-col  w-screen">
+      <div className="md:py-10 md:px-20 flex flex-col w-screen">
         <h1 className="text-3xl text-center md:text-4xl font-bold">
           Send Messages to unknown usernames !!
         </h1>
         <p className="text-xl mt-8">
-          send anyonimous message to <a className="font-bold">{usernmae}</a>
+          Send anonymous message to <span className="font-bold">{username}</span>
         </p>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <textarea className="mt-5 w-full border-solid border-2 border-gray-500 py-3 px-3" rows={5} cols={5} placeholder="Write A messafe" {...register("textArea" , { required: true, minLength: 8 })} />
-          {errors.textArea && <span>This field is required</span>}
-          
+          <textarea
+            className="mt-5 w-full border-solid border-2 border-gray-500 py-3 px-3"
+            rows={5}
+            cols={5}
+            placeholder="Write A message"
+            {...register("textArea", { required: true, minLength: 8 })}
+          />
+          {errors.textArea && <span className="text-red-500 block">This field is required and must be at least 8 characters long</span>}
+
           <button
-            type="button"
-            onClick={() => {
-              setValue("textArea", ""); 
-            }}
+            type="submit"
             className="bg-slate-900 text-white px-5 py-1 rounded-lg"
-          > Send
             
+          >
+            Send
           </button>
         </form>
       </div>
