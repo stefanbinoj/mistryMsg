32ee8bed996d10aaee4d22608b775400e80143a0
stefanbinoj <stefan.binoj.007@gmail.com>
2024-12-11 15:06:19 +0530

    update home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -26,6 +26,8 @@ export function SkeletonDemo() {
 }
 
 const page = () => {
+  const savedTheme : string  = localStorage.getItem('theme') || 'white';
+    
   const [loading , setloading] = useState(true)
   useEffect(()=>{
     setloading(true)
@@ -39,19 +41,25 @@ const page = () => {
     <>{ loading ? (<SkeletonDemo />):(<>
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-10 h-min-screen">
         <section className="text-center mb-8 md:mb-12">
-          <h1 className="text-3xl md:text-5xl font-bold shadow-lg"
-            style={{ textShadow: "1px 30px 4px rgba(255, 255, 255, 0.4)" , transition: "text-shadow 10s ease-in-out" }}          >
-            Dive into the World of Anonymous Feedback
-          </h1>
-          <p className="mt-5 md:mt-7 text-base md:text-lg">
+        <h1
+  className="text-3xl md:text-5xl font-bold shadow-lg"
+  style={savedTheme === 'dark'
+    ? { textShadow: "1px 30px 4px rgba(255, 255, 255, 0.4)", transition: "text-shadow 10s ease-in-out", color: '#e0e0e0' }  // Dark theme styles with lighter text color
+    : { textShadow: "1px 30px 4px rgba(0, 0, 0, 0.4)", transition: "text-shadow 10s ease-in-out", color: '#333333' } // Light theme styles with darker text color
+  }
+>
+  Dive into the World of Anonymous Feedback
+</h1>
+
+          <p className="mt-10 md:mt-17 text-base md:text-xl font-bold">
             True Feedback - Where your identity remains a secret.
           </p>
         </section>
         
 
         <Carousel
-        plugins={[AutoPlay({delay:1500})]}
-        className="w-full max-w-xs">
+        plugins={[AutoPlay({delay:2000})]}
+        className=" max-w-xs ">
       <CarouselContent>
         {
           messages.map((msg , index)=>{
