6f2dcd2740fadcb2c544188004b3b6bdefe5bf3d
stefanbinoj <stefan.binoj.007@gmail.com>
2024-12-11 15:16:54 +0530

    lint update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,10 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { useSession , signOut } from 'next-auth/react'
 import { User } from 'next-auth'
-import { Button } from './ui/button'
-
-import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
 import { useToast } from '@/hooks/use-toast'
+import Link from 'next/link'
 
 const Navbar = () => {
   const {toast} = useToast();
@@ -55,7 +52,7 @@ const Navbar = () => {
     <nav className={`pl-5 pr-0 py-5 mx-2 z-50 shadow-lg sticky top-0 backdrop-blur-sm border-b  ${isDarkMode ? 'bg-black/30 text-white border-gray-800' : 'bg-white text-black border-gray-300'}`}>
         <div className='container w-full 
         md:flex flex-row gap-4 items-center ml-5 w-screen justify-between '>
-            <a className={`font-bold text-xl ml-4 ${!isDarkMode?'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text':'bg-gradient-to-r from-orange-400 to-red-400 text-transparent bg-clip-text'} hover:scale-125 transition-all duration-300 ease-in-out`} href='/'>Mystery Message</a>
+            <Link className={`font-bold text-xl ml-4 ${!isDarkMode?'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text':'bg-gradient-to-r from-orange-400 to-red-400 text-transparent bg-clip-text'} hover:scale-125 transition-all duration-300 ease-in-out`} href='/'>Mystery Message</Link>
             <div className=' hidden md:flex md:flex-row md:block w-3/5 gap-4 ml-auto mr-0 justify-end'>
               
               <a className=' cursor-pointer hover:scale-125 transition-all hover:font-bold duration-300 ease-in-out' href='/dashboard'>Dashboard</a>
