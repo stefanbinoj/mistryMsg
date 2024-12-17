ac7fea075ef8c2e1a2ddae77596e5a516792af58
stefanbinoj <stefan.binoj.007@gmail.com>
2024-12-17 18:19:39 +0530

    max-width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <AuthProvider>
         <NuqsAdapter>
           <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased element`}
           >
             {/* <Navbar/> */}
             {children}
